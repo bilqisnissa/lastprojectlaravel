f236f2759958ea86994bc7eb252f55ffb4775657
Bilqis <hirosasi01@gmail.com>
2021-05-26 11:22:38 +0700

    Last Project

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\PhoneController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+//GET untuk mendapatkan user by id
+Route::get('user/{id}', [UserController::class, 'getUserById']);
+
+//GET untuk mendapatkan semua user
+Route::get('users', [UserController::class, 'getAllUsers']);
+
+//POST untuk menambahkan data user
+Route::post('users', [UserController::class, 'createUser']);
+
+//POST untuk mengupdate data user
+Route::post('user/{id}', [UserController::class, 'updateUser']);
+
+Route::post('users/photo/{id}', [UserController::class, 'addPhotoProfile']);
+
+//GET untuk mendapatkan data telpon
+Route::get('users/{user_id}/phone', [PhoneController::class, 'getAllPhone']);
+
+//POST untuk menambahkan data telpon
+Route::post('users/{user_id}/phone', [PhoneController::class, 'createPhone']);
+
+//DELETE untuk menghapus data telpon
+Route::delete('users/{user_id}/phone/{id}', [PhoneController::class, 'deletePhone']);
